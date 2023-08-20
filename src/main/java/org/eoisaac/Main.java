7524b83b76cb79b23a0fcbc339b43a62e4996357
Isaac Santiago <eoisaacc@gmail.com>
2023-08-19 23:31:49 -0300

    feat(refactor): create TransactionFormView in AppFrame

--- src/main/java/org/eoisaac/Main.java
+++ src/main/java/org/eoisaac/Main.java
@@ -1,15 +1,48 @@
 package org.eoisaac;
 
 
-import org.eoisaac.views.AppFrame;
-
 import javax.swing.*;
+import org.eoisaac.model.dao.TransactionDao;
+import org.eoisaac.views.AppFrame;
 
 public class Main {
   public static void main(String[] args) {
+    TransactionDao transactionDao = new TransactionDao();
+//
+//    TransactionEntity newTransaction =
+//            TransactionEntity.builder()
+//                    .name("Sample Transaction 2")
+//                    .type(TransactionType.EXPENSE)
+//                    .value(100.0f)
+//                    .entryDate(Instant.now())
+//                    .build();
+//
+//    Optional<TransactionEntity> created = transactionDao.create(newTransaction);
+//    if (created.isPresent()) {
+//      TransactionEntity createdTransaction = created.get();
+//      createdTransaction.setName("Updated Transaction Name");
+//      TransactionEntity updatedTransaction = transactionDao.update(createdTransaction);
+//      if (updatedTransaction != null) {
+//        System.out.println("Transaction updated successfully: " + updatedTransaction.getId());
+
+//        boolean deleted = transactionDao.delete(updatedTransaction);
+//        if (deleted) {
+//          System.out.println("Transaction deleted successfully.");
+//        } else {
+//          System.out.println("Failed to delete transaction.");
+//        }
+
+//      } else {
+//        System.out.println("Failed to update transaction.");
+//      }
+//    } else {
+//      System.out.println("Failed to create transaction.");
+//    }
+
     SwingUtilities.invokeLater(() -> {
       AppFrame appFrame = new AppFrame();
       appFrame.setVisible(true);
     });
   }
 }
+
--- src/main/java/org/eoisaac/model/dao/CategoryDao.java
+++ src/main/java/org/eoisaac/model/dao/CategoryDao.java
@@ -1,3 +1,61 @@
 package org.eoisaac.model.dao;
 
-public class CategoryDao {}
+import java.util.Optional;
+import org.eoisaac.config.database.DatabaseSession;
+import org.eoisaac.model.entities.CategoryEntity;
+import org.hibernate.Session;
+
+public class CategoryDao {
+
+  public Optional<CategoryEntity> create(CategoryEntity entity) {
+    try (Session session = DatabaseSession.get()) {
+      if (session != null) {
+        session.beginTransaction();
+        session.persist(entity);
+        session.getTransaction().commit();
+        return Optional.of(entity);
+      } else {
+        System.out.println("Session not created");
+        return Optional.empty();
+      }
+    } catch (Exception e) {
+      return Optional.empty();
+    }
+  }
+
+  public CategoryEntity update(CategoryEntity entity) {
+    Session session = null;
+    try {
+      session = DatabaseSession.get();
+      if (session != null) {
+        session.beginTransaction();
+        session.merge(entity);
+        session.getTransaction().commit();
+        return entity;
+      } else {
+        System.out.println("Session not created");
+        return null;
+      }
+    } finally {
+      DatabaseSession.close(session);
+    }
+  }
+
+  public Boolean delete(CategoryEntity entity) {
+    Session session = null;
+    try {
+      session = DatabaseSession.get();
+      if (session != null) {
+        session.beginTransaction();
+        session.remove(entity);
+        session.getTransaction().commit();
+        return true;
+      } else {
+        System.out.println("Session not created");
+        return false;
+      }
+    } finally {
+      DatabaseSession.close(session);
+    }
+  }
+}
